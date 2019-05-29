cb02a5286d397f0fde2a34ca56453c0d512dead3
zhaoao <1029361539@qq.com>
2019-05-29 12:01:09 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <el-button type="primary">主要按钮</el-button>
+    <!-- <i class="el-icon-edit"></i> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  // name: "home",
+};
+</script>
